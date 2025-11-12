bd3f3161f39c1b726c205b07f2e78c54830c7ff0
Anderson Pinto <andersonaugustopinto@gmail.com>
2025-11-11 23:47:10 -0300

    Update title in index.html and adjust image paths in palestra.ts and Home.tsx for consistency.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ export function Home() {
             </div>
           </div>
           <div className={styles.illustration}>
-            <img src="../public/images/templates/seguranca-trabalho.png" alt="Seguranca" style={{ width: '700px', height: '500px', borderRadius: '10%' }} />
+            <img src="/images/templates/seguranca-trabalho.png" alt="Seguranca" style={{ width: '700px', height: '500px', borderRadius: '10%' }} />
           </div>
         </div>
       </section>
